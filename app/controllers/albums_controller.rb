2512feac3156236156f7da54cd97215b3c1bbace
JustusFT <JustusFT@gmail.com>
2017-05-30 15:58:25 +0800

    All basic routes and actions for album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,57 @@
 class AlbumsController < ApplicationController
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(strong_params)
+    @album.user = helpers.current_user
+    if @album.save
+      flash[:notice] = "album create successfully"
+      redirect_to @album
+    else
+      flash[:error] = "album create failed"
+      render :new
+    end
+  end
+
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    @album.update_attributes(strong_params)
+    if @album.save
+      flash[:notice] = "album create successfully"
+      redirect_to @album
+    else
+      flash[:error] = "album create failed"
+      render :new
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    if @album.destroy
+      flash[:notice] = "album destroyed successfully"
+      redirect_to "/"
+    else
+      flash[:error] = "album destroyed failed"
+      redirect_to @album
+    end
+  end
+
+  private
+  def strong_params
+    params.require(:album).permit(:title, :description)
+  end
 end
